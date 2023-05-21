4c8470251fdc773313f8871945a6f62c6edff18e
Gunnar Batista <gunnar-batista@hotmail.com>
2023-05-21 14:46:50 -0300

    First commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<header class="header">
+    <div class="header-title">
+      <a href="index.php"><img alt="Imaterial: cultura, artesanto e história " class="logo" src="src/logo.png"></a>
+    </div>
+    <div class="content-buttons">
+      <ul>
+        <li onclick="location.href='produtos.php'" class="header-buttons"><button class="btn-header">Produtos</button>
+        </li>
+        <li onclick="location.href='contact.php'" class="header-buttons"><button class="btn-header">Contato</button>
+        </li>
+        <li onclick="location.href='about.php'" class="header-buttons"><button class="btn-header">Sobre</button></li>
+        <li onclick="location.href='cart.php'" class="header-buttons"><button class="btn-header">Carrinho</button></li>
+      </ul>
+    </div>
+  </header>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-BR"><head>
+  <link rel="stylesheet" href="style.css">
+  <title>Imaterial</title>
+  <link rel="Website Icon" type="png" href="src/logo.png">
+</head>
+<body>
+  <?php include 'header.php'?>
+  <main>
+    <div class="content">
+      <div class="content-title">
+        <h2>PRODUTOS EM DESTAQUE</h2>
+      </div>
+      <div class="content-body">
+        <ul class="list-itens">
+          <li class="card-index">
+            <div class="card-head">
+              <div class="product-content-img">
+                <img class="product-img" src="./src/Products/product1.png">
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="product-desc">
+
+              </div>
+              <div class="product-properties">
+                <span class="product-price">
+              </span></div>
+              <div>
+                <button>Adicionar ao carrinho</button>
+              </div>
+            </div>
+          </li>
+          <li class="card-index">
+            <div class="card-head">
+              <div class="product-content-img">
+                <img class="product-img" src="./src/Products/product2.png">
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="product-desc">
+
+              </div>
+              <div class="product-properties">
+                <span class="product-price">
+              </span></div>
+              <div>
+                <button>Adicionar ao carrinho</button>
+              </div>
+            </div>
+          </li>
+          <li class="card-index">
+            <div class="card-head">
+              <div class="product-content-img">
+                <img class="product-img" src="./src/Products/product3.png">
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="product-desc">
+
+              </div>
+              <div class="product-properties">
+                <span class="product-price">
+              </span></div>
+              <div>
+                <button>Adicionar ao carrinho</button>
+              </div>
+            </div>
+          </li>
+          <li class="card-index">
+            <div class="card-head">
+              <div class="product-content-img">
+                <img class="product-img" src="./src/Products/product4.png">
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="product-desc">
+              </div>
+              <div class="product-properties">
+                <span class="product-price">R$: 250,00</span>
+              </div>
+              <div>
+                <button>Adicionar ao carrinho</button>
+              </div>
+              
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="content">
+        <h1>MELHOR LOJA DA REGIÃO</h1>
+    </div>
+  </main>
+<?php include 'footer.php'>
+</body>
+</html>
